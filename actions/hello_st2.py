f5f138dd3d79b32ee81bcbae7ca164a8d46ec51d
lee <gp@nerve4.com>
2023-09-14 14:36:58 +0100

    feat

---
+++ actions/hello_st2.py
@@ -0,0 +1,6 @@
+from st2common.runners.base_action import Action
+
+class HelloStackStorm(Action):
+    def run(self):
+        print("Hello Stackstorm, want to be friends?")
+        return True, "Let's be friends!"
